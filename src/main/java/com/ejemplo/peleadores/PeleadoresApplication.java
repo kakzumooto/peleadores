a3f1bcc86dda0b09ec166685118dcffebfe6a850
kakzumooto <kakzumooto22@gmail.com>
2025-02-07 00:06:11 -0600

    Inicializando el proyecto de Peleadores

---
+++ src/main/java/com/ejemplo/peleadores/PeleadoresApplication.java
@@ -0,0 +1,13 @@
+package com.ejemplo.peleadores;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeleadoresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeleadoresApplication.class, args);
+	}
+
+}
